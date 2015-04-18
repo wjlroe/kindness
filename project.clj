7d319baa14d5b04717efda1406bdc8f4052da9d0
William Roe <git@wjlr.org.uk>
2015-04-18 11:02:58 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,40 @@
+(defproject kindness "0.1.0-SNAPSHOT"
+  :description "Ludum Dare 32 Entry: An Unconventional Weapon"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2850"]
+                 [figwheel "0.2.5-SNAPSHOT"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+    :node-dependencies [[source-map-support "0.2.8"]]
+
+  :plugins [[lein-cljsbuild "1.0.4"]
+            [lein-npm "0.4.0"]
+            [lein-figwheel "0.2.5-SNAPSHOT"]]
+
+  :source-paths ["src" "target/classes"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"]
+
+  :cljsbuild {
+              :builds [{:id "dev"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :main kindness.core
+                                   :output-to "resources/public/js/compiled/out/kindness.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :optimizations :none
+                                   :cache-analysis true
+                                   :source-map true
+                                   :asset-path "js/compiled/out"}}
+                       {:id "release"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :main kindness.core
+                                   :output-to "resources/public/js/compiled/out-adv/kindness.min.js"
+                                   :output-dir "resources/public/js/compiled/out-adv"
+                                   :optimizations :advanced
+                                   :pretty-print false}}]}
+
+  :figwheel {:http-server-root "public"})
---
+++ src/kindness/core.clj
@@ -0,0 +1,6 @@
+(ns kindness.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
